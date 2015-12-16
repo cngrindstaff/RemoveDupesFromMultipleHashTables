312e9342a1737aef7e90dcb790fbe5d0289f34cb
chelseagrindstaff <chelseagrindstaff@gmail.com>
2015-12-16 16:02:13 -0500

    I waited too long to commit this and now I forgot what I did...

--- RemoveDupesFromMultipleHashTables/Program.cs
+++ RemoveDupesFromMultipleHashTables/Program.cs
@@ -5,23 +5,33 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace RemoveDupesFromMultipleHashTables
 {
     class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger("RemoveDupesFromHashTablesLog");
+        
         static void Main(string[] args)
         {
             List<string> filePathList = new List<string>();
 
-            string stationListCsv = ConfigurationManager.AppSettings["stationList"];
-            List<string> stationList = stationListCsv.Split(',').ToList();
+            List<string> stationList = GetListFromFile(ConfigurationManager.AppSettings["AllStationsWithProcessedFilePaths"]);
+            Log.Info("Station List: " + string.Join(",", stationList));
 
             string filePathStructure = ConfigurationManager.AppSettings["FilePathStructure"];
 
             foreach (string station in stationList)
             {
                 string fullPath = filePathStructure.Replace("[station]", station.ToLower());
+
+                if (!File.Exists(fullPath))
+                {
+                    fullPath = fullPath.Replace(@"Tegnavision Archiver\",
+                        @"Tegnavision Archiver\Completed\");
+                }
+
                 filePathList.Add(fullPath);
             }
 
@@ -29,6 +39,7 @@ namespace RemoveDupesFromMultipleHashTables
 
             foreach (string file in filePathList)
             {
+                Log.Info("Getting processed list from " + file);
                 Dictionary<string, string> thisStationsDictionary = ReadDictionaryItems(file);
                 Dictionary<string, string> uniqueValuesToWriteToFile = new Dictionary<string, string>();
 
@@ -38,6 +49,10 @@ namespace RemoveDupesFromMultipleHashTables
                     {
                         allProcessedHtDictionary.Add(kvp.Key, kvp.Value);
                         uniqueValuesToWriteToFile.Add(kvp.Key, kvp.Value);
+                    }
+                    else
+                    {
+                        Log.Info("Key " + kvp.Key + " was already in another Hash Table. Deleting from " + file);
                     }
                 }
                 File.WriteAllLines(file, uniqueValuesToWriteToFile.Select(x => x.Key + ";" + x.Value).ToArray());
@@ -59,34 +74,74 @@ namespace RemoveDupesFromMultipleHashTables
                     foreach (string line in lines)
                     {
                         string[] keyValuePair = line.Split(';');
-                        if (!table.ContainsKey(keyValuePair[0]))
+                        if (!table.ContainsKey(keyValuePair[0]) && !string.IsNullOrEmpty(keyValuePair[1]))
                         {
                             table.Add(keyValuePair[0], keyValuePair[1]);
                         }
                         else
                         {
-                            //Log.Error("Hashtable already contains key " + keyValuePair[0].ToString());
+                            Log.Error("Hashtable already contains key " + keyValuePair[0].ToString());
                         }
                         if (string.IsNullOrEmpty(keyValuePair[1]))
                         {
-                            //Log.Error("The key " + keyValuePair[0] + " does not have an associated value.");
+                            Log.Error("The key " + keyValuePair[0] + " does not have an associated value.");
                         }
                     }
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Error Reading Dictionary Items from " + location);
+                    Log.Error("Error Reading Dictionary Items from " + location);
                 }
 
             }
             else
             {
             }
-            //Log.Info("Found " + table.Count + " processed items in the  " + location + " dictionary.");
-
-            Console.WriteLine("Found " + table.Count + " Items from " + location);
+            Log.Info("Found " + table.Count + " Items from " + location);
             return table;
         }
+
+
+
+        public static List<string> GetListFromFile(string filePath)
+        {
+            Log.Info("Trying to get station list from " + filePath);
+            List<string> newList = new List<string>();
+            
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    using (StreamReader r = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = r.ReadLine()) != null)
+                        {
+                            newList.Add(line);
+                        }
+                    }
+                    int numberOfItems = newList.Count();
+                    if (numberOfItems == 0)
+                    {
+                        Log.Info("No items in " + filePath);
+                    }
+                    else
+                    {
+                        Log.Info("Found " + numberOfItems + " from " + filePath);
+                    }
+                    return newList;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error("Could not generate list of items from the selected file. Error: " + ex.Message +
+                              ", Inner Exception:" + ex.InnerException);
+                    return newList;
+                }
+            }
+            return newList;
+
+        } 
+
 
     }
 }
--- RemoveDupesFromMultipleHashTables/Properties/AssemblyInfo.cs
+++ RemoveDupesFromMultipleHashTables/Properties/AssemblyInfo.cs
@@ -5,6 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using log4net.Config;
+
 [assembly: AssemblyTitle("RemoveDupesFromMultipleHashTables")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -13,6 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: XmlConfigurator()]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
